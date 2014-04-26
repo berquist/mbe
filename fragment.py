82f46a20672bc3525de760184e5d8155b49b7adf
Eric Berquist <erb74@pitt.edu>
2014-04-26 10:57:25 -0400

    Initial version of the fragment class.

---
+++ fragment.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import os
+import sympy
+
+
+class Fragment(object):
+    """
+    A fragment is a molecule that can be combined with another
+    fragment.
+    """
+    def __init__(self):
+        self.fxyz = ""
+        self.name = ""
+        self.comment = ""
+        self.charge = 0
+        self.multiplicity = 1
+        self.atoms = []
+        self.coords = []
+        self.nfragments = 0
+        self.active = False
+        self.symbol_repr = []
+
+    def is_active(self):
+        """
+        Is this fragment going to be EPR-active? Determine based
+        on the multiplicity.
+        """
+        if self.multiplicity == 1:
+            return False
+        elif self.multiplicity == 2:
+            return True
+        else:
+            raise Exception('Only singlets and doublets are allowed.')
+
+    def read(self, fxyz):
+        """
+        Load the charge, multiplicity, and coordinates into the fragment.
+        """
+        self.fxyz = fxyz
+        self.name = os.path.splitext(os.path.basename(fxyz))[0]
+        xyzhandle = open(fxyz, "r")
+        xyzhandle.readline()
+        self.charge, self.multiplicity \
+            = list(map(int, xyzhandle.readline().split()))
+        raw_coords = xyzhandle.readlines()
+        xyzhandle.close()
+        for line in raw_coords:
+            if line != '\n':
+                self.atoms.append(line.split()[0])
+                self.coords.append(list(map(float, line.split()[1:])))
+        self.nfragments += 1
+        self.active = self.is_active()
+        self.symbol_repr.append(sympy.S(self.name))
+
+    def write(self, fxyz):
+        """
+        Write an XYZ file to disk, with the charge and multiplicity stored
+        in the comment line.
+        """
+        stemp = "{:2s} {:f} {:f} {:f}\n"
+        with open(fxyz, "w") as xyzhandle:
+            for atom, coords in zip(self.atoms, self.coords):
+                xyzhandle.write(stemp.format(atom, *coords))
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        return self.combine(other)
+
+    def combine(self, other):
+        """
+        Combine this fragment with another fragment, returning a new fragment.
+        """
+        new = Fragment()
+        new.name = other.name + self.name
+        new.charge = other.charge + self.charge
+        if other.multiplicity == self.multiplicity:
+            new.multiplicity = 1
+        else:
+            new.multiplicity = 2
+        new.atoms = other.atoms + self.atoms
+        new.coords = other.coords + self.coords
+        new.nfragments = other.nfragments + self.nfragments
+        new.active = other.active ^ self.active
+        new.symbol_repr = other.symbol_repr + self.symbol_repr
+        return new
+
+    def combine_write(self, other, fxyz):
+        """
+        Combine this fragment with another fragment, returning a new fragment
+        and writing it to disk.
+        """
+        new = self.combine(other)
+        new.write(fxyz)
+        return new
