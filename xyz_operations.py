804d13e2a2b033af6e8a2c30d40dce308859d0c1
Eric Berquist <erb74@pitt.edu>
2014-04-26 10:40:44 -0400

    2to3 and some pep8 fixes.

--- xyz_operations.py
+++ xyz_operations.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 def read_xyz(filename):
     """
     A simple example for reading from an XYZ file line by line, not all at once
@@ -13,9 +14,10 @@ def read_xyz(filename):
         for line in xyzfile:
             if len(line) > 1:
                 atoms.append(line.split()[0])
-                coords.append(map(float, line.split()[1:]))
+                coords.append(list(map(float, line.split()[1:])))
 
     return (natoms, comment, atoms, coords)
+
 
 def read_fragment_xyz(filename):
     """
@@ -31,15 +33,18 @@ def read_fragment_xyz(filename):
     frag_coords = []
     with open(filename, 'r') as fragfile:
         # the very first line is the system's total charge and multiplicity
-        sys_charge, sys_multiplicity = map(int, fragfile.readline().split())
+        sys_charge, sys_multiplicity \
+            = list(map(int, fragfile.readline().split()))
         for line in fragfile:
             # each fragment section is separated by '--' as a delimiter
             # if we find it, we're at the start of a new block
             if line[0:2] == '--':
                 comment = " ".join(line.split()[1:])
                 comments.append(comment)
-                # the fragment's own charge and multiplicity must come immediately afterwards
-                frag_charge, frag_multiplicity = map(int, next(fragfile).split())
+                # the fragment's own charge and multiplicity
+                # must come immediately afterwards
+                frag_charge, frag_multiplicity \
+                    = list(map(int, next(fragfile).split()))
                 frag_charges.append(frag_charge)
                 frag_multiplicities.append(frag_multiplicity)
                 # reset the fragment atoms and coordinates after append
@@ -53,32 +58,40 @@ def read_fragment_xyz(filename):
             # must be a fragment's atom entry
             else:
                 atom = line.split()[0]
-                atom_coords = map(float, line.split()[1:])
+                atom_coords = list(map(float, line.split()[1:]))
                 frag_atoms.append(atom)
                 frag_coords.append(atom_coords)
-        # these need to be here otherwise the last fragment will never be appended
+        # these need to be here otherwise the last fragment
+        # will never be appended
         atoms.append(frag_atoms)
         coords.append(frag_coords)
 
-    return (sys_charge, sys_multiplicity, frag_charges, frag_multiplicities, atoms, coords, comments)
+    return (sys_charge, sys_multiplicity, frag_charges, frag_multiplicities,
+            atoms, coords, comments)
+
 
 def write_individual_fragments(filename):
     """
     Read a combined fragment XYZ file, split it up into individual fragment XYZ
     files, and write them to disk.
     """
-    sys_charge, sys_multiplicity, frag_charges, frag_multiplicities, atoms, coords, comments = read_fragment_xyz(filename)
+    sys_charge, sys_multiplicity, frag_charges, frag_multiplicities, \
+        atoms, coords, comments = read_fragment_xyz(filename)
     # the number of fragments is the length of any of the returned lists
     nfragments = len(frag_charges)
-    # template for individual fragment filename(s)
-    s = os.path.splitext(filename)[0] + '.F{}.xyz'
+    # string templates
+    sf = os.path.splitext(filename)[0] + '.F{}.xyz'
+    sc = '{} {} {}\n'
+    sa = '{:3} {:12.7f} {:12.7f} {:12.7f}\n'
     # generate a single XYZ file for each fragment
     for f in range(nfragments):
-        with open(s.format(f), 'w') as xyzfile:
+        with open(sf.format(f), 'w') as xyzfile:
             xyzfile.write('{}\n'.format(len(atoms[f])))
-            xyzfile.write('{} {} {}\n'.format(frag_charges[f], frag_multiplicities[f], comments[f]))
+            xyzfile.write(sc.format(frag_charges[f],
+                                    frag_multiplicities[f],
+                                    comments[f]))
             for atom in range(len(atoms[f])):
-                xyzfile.write('{:3} {:12.7f} {:12.7f} {:12.7f}\n'.format(atoms[f][atom], *coords[f][atom]))
+                xyzfile.write(sa.format(atoms[f][atom], *coords[f][atom]))
 
 if __name__ == '__main__':
     import os
