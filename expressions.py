2b40d303cf6078bb5c61f98f74aac307af328fd7
Eric Berquist <erb74@pitt.edu>
2014-05-26 17:46:02 -0400

    A semi-recursive function that finally works.

--- expressions.py
+++ expressions.py
@@ -1,118 +1,26 @@
 #!/usr/bin/env python
 
+from sympy import prod
+from itertools import combinations
 
-def term2str(term):
-    """
-    Convert a term (sympy.core.mul.Mul) into its string representation
-    by splitting and joining at each multiplication.
-    """
-    s = str(term).replace('*', '')
-    return s
+def dEn(f, n):
+    """Generate a single n-body term."""
+    if n < 1:
+        return 0
+    elif n == 1:
+        return sum(f)
+    else:
+        acc = prod(f)
+        for i in range(1, n):
+            acc -= sum(dEn(g, i) for g in combinations(f, i))
+        return acc
 
-
-def gen_dict_term2str(termdict):
-    """
-    Given a dictionary where all the keys are terms, return a new dictionary
-    that maps each term to its custom string form.
-    """
-    newdict = dict()
-    for term in termdict:
-        newdict[term] = term2str(term)
-    return newdict
-
-
-def invert_map(m):
-    """
-    Return the map m, inverted.
-    """
-    im = {v: k for k, v in m.items()}
-    return im
-
-
-def gen_dict_symbol2monomer(monomers):
-    """
-    Given a sequence of monomers, create a map of monomer symbols
-    to monomer objects.
-    """
-    newdict = dict()
-    for monomer in monomers:
-        newdict[next(iter(monomer.symbol_repr))] = monomer
-    return newdict
-
-
-def T1(f):
-    """
-    Generate one-body terms.
-    """
-    n = len(f)
-    terms = 0
-    for i in range(n):
-        terms += f[i]
-    return terms
-
-
-def T2(f):
-    """
-    Generate two-body terms.
-    """
-    n = len(f)
-    terms = 0
-    for i in range(n):
-        for j in range(i+1, n):
-            terms += (f[i]*f[j]) - f[i] - f[j]
-    return terms
-
-
-def T3(f):
-    """
-    Generate three-body terms.
-    """
-    n = len(f)
-    terms = 0
-    for i in range(n):
-        for j in range(i+1, n):
-            for k in range(j+1, n):
-                terms += ((f[i]*f[j]*f[k] - f[i] - f[j] - f[k]) -
-                          (f[i]*f[j] - f[i] - f[j]) -
-                          (f[j]*f[k] - f[j] - f[k]) -
-                          (f[k]*f[i] - f[k] - f[i]))
-    return terms
-
-
-def Tn(f, n):
-    """
-    Generate N-body terms.
-    """
-    terms = 0
-
-    return terms
-
-
-def MBE1(f):
-    """
-    Generate the full 1st-order many-body expansion expression.
-    """
-    return T1(f)
-
-
-def MBE2(f):
-    """
-    Generate the full 2nd-order many-body expansion expression.
-    """
-    return MBE1(f) + T2(f)
-
-
-def MBE3(f):
-    """
-    Generate the full 3rd-order many-body expansion expression.
-    """
-    return MBE2(f) + T3(f)
-
+def En(f, n):
+    """Generate all possible n-body terms."""
+    return sum(dEn(g, n) for g in combinations(f, n))
 
 if __name__ == '__main__':
-    from sympy import var, I, symbols
-    x, y = var('x, y')
-    g = 3*I+5*x**2 + 7*y + 10*x*y
+    from sympy import symbols
 
     f2 = list(symbols('frag0:2'))
     f3 = list(symbols('frag0:3'))
