44548d4466a9ebe114edb51b9a85e8ffecf7f5ab
Eric Berquist <eric.berquist@gmail.com>
2020-05-19 23:15:19 -0400

    Remove unnecessary __init__ imports

--- mbe/__init__.py
+++ mbe/__init__.py
@@ -1,11 +1,2 @@
 """mbe: Tools for performing many-body expansions using computational
 chemistry packages."""
-
-from . import epr
-from . import expressions
-from . import expressions_explicit
-from . import file_templates
-from . import fragment
-from . import pbs
-from . import utils
-from . import xyz_operations
